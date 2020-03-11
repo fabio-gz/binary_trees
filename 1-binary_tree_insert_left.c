942b79e147df55abffd636857e259c2dc41d6e36
fabio-gz <fabiogomez.silva@gmail.com>
2020-03-11 23:43:18 +0000

    [Fix]parent null condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 
+	if (parent == NULL)
+		return (NULL);
+
 	if (parent->left == NULL)
 	{
 		new->parent = parent;
